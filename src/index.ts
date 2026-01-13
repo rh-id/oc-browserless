c8255e02123bc5b4e8e2fc6fb6678084d5df3a59
Ruby Hartono <58564005+rh-id@users.noreply.github.com>
2026-01-13 22:40:10 +0700

    refactor: remove unused code and scripts
    - Remove dev script from package.json (plugins cannot be run)
    - Delete src/index.ts (unused re-export file)

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export { BrowserlessPlugin } from './plugin/browserless.js';
-export type { BrowserlessOptions } from './browser/manager.js';
-export { getBrowserManager, resetBrowserManager } from './browser/manager.js';
-export { isValidUrl } from './utils/common.js';
